11aab2c0af91ea36ff5288391d14d48b0e7da7a3
Rahul <109830797+rahul-269@users.noreply.github.com>
2023-07-25 21:43:14 +0400

    all test cases cleared

--- TwoSum.js
+++ TwoSum.js
@@ -5,20 +5,17 @@
  */
 var twoSum = function(nums, target) {
 
-    let sum = 0; // to check if equal to target
-    let indices =[]; // array to be populated for return
-
+    let sum = 0;
+    let indices =[]; 
     for( i = 0 ; i < nums.length ; i++)
     {
-      if(i != 0) 
+      for ( j = i+1 ; j< nums.length ; j++)
       {
-        sum = nums[i]+nums[i-1];
-        
+        sum = nums [i] + nums [j];
         if(sum===target)
         {
-        
-        indices.push(i-1); // push position of first element into indices array
-        indices.push(i); // push position of second element into indices array
+          indices.push(i);
+          indices.push(j);
         }
       }
     }
