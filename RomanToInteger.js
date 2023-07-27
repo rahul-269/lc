c0e494db573d493418703d4f08f8fef40bb7cf17
Rahul <109830797+rahul-269@users.noreply.github.com>
2023-07-27 05:25:45 +0400

    better runtime and memory

--- RomanToInteger.js
+++ RomanToInteger.js
@@ -3,81 +3,34 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const rsym = { 
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
 
     let value = 0;
-    if(s.length === 1)
+
+    for ( i = s.length-1 ; i>=0 ; --i)
     {
-        if( s === "I")
-        {
-            value = value + 1;
-        }
-        else if( s === "V")
-        {
-            value = value + 5;
-        }
-        else if( s === "X")
-        {
-            value = value + 10;
-        }
-        else if( s === "L")
-        {
-            value = value + 50;
-        }
-        else if( s === "C")
-        {
-            value = value + 100;
-        }
-        else if( s === "D")
-        {
-            value = value + 500;
-        }
-        else
-        {
-            value = value + 1000;
-        }
-    }
+       let current = rsym[s[i]];
+       let successor = 0;
+       if(i+1 <= s.length-1)
+       {
+         successor = rsym[s[i+1]]; 
+       }
 
-    else { 
-        for( i  = s.length-1 ; i >= 0 ; --i)
-        {
-            if( s[i] === "I")
-        {   if(s[i+1]==="V" || s[i+1]==="X")
-            value = value - 1;
-            else
-            value = value + 1;
-        }
-        else if( s[i] === "V")
-        {
-            value = value + 5;
-        }
-        else if( s[i] === "X")
-        {
-            if(s[i+1]==="L" || s[i+1]==="C")
-            value = value - 10;
-            else
-            value = value + 10;
-        }
-        else if( s[i] === "L")
-        {
-            value = value + 50;
-        }
-        else if( s[i] === "C")
-        {
-            if(s[i+1]==="D" || s[i+1]==="M")
-            value = value - 100;
-            else
-            value = value + 100;
-        }
-        else if( s[i] === "D")
-        {
-            value = value + 500;
-        }
-        else
-        {
-            value = value + 1000;
-        }   
-        }
-    }
+       if(current < successor)
+       {
+           value = value - current;
+       }
+       else
+       value = value + current;
 
+    }
     return value;
-    }
+};
