7714aaf740660c7b87b1ae35488376aca5440712
Rahul <109830797+rahul-269@users.noreply.github.com>
2023-07-30 08:33:44 +0400

    Update RemoveDuplicatesFromSortedArray.js

--- RemoveDuplicatesFromSortedArray.js
+++ RemoveDuplicatesFromSortedArray.js
@@ -3,16 +3,19 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let k = 0;
-    for( i = 0 ; i< nums.length ; i++)
+
+    if(nums.length <=1)
+    return nums.length;
+    else
+    { for( i = 0 ; i< nums.length ; i++)
     { 
-        k = k+1;
         for( j = i+1 ; j< nums.length ; j++)
             if(nums[i] === nums[j])
             {
-                nums.splice(j,1);
-                --j; 
+                nums.splice(j,1); // remove one item from position j
+                --j; //decrement j to reiterate over same position because different value is at that position now
             }
     }
-    return k ;
+    return nums.length;
+    }
 };
